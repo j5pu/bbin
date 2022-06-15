39ddccb285cf647faae5a8ac94b4e1e7ca67aaa1
root <root@example.com>
2022-06-15 22:10:33 +0200

    moving to profile/profile.sh

--- libexec/utils.sh
+++ libexec/utils.sh
@@ -4,29 +4,6 @@
 # Posix Utils Library
 
 #######################################
-# has alias, command or function
-# Arguments:
-#   1   alias, command or function name
-# Returns:
-#   1   parameter null or not set
-#######################################
-has() { command -v "${1?}" >/dev/null; }
-
-#######################################
-# sources all files in the first level of a directory, including hidden files
-# Arguments:
-#  directory    path of directory to source (default: cwd).
-#######################################
-source_dir () {
-  if dir-has-files "${1:-.}"; then
-    for _source_dir_file in "${1:-.}"/*; do
-      . "${_source_dir_file}"
-    done
-    unset _source_dir_file
-  fi
-}
-
-#######################################
 # change to git repository top path
 # Arguments:
 #  None
