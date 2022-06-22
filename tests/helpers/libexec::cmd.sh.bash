b19eeecde184803a3a1534ef1c737dea339ffad8
root <root@example.com>
2022-06-22 17:18:52 +0200

    feat: cd.sh

--- tests/helpers/libexec::cmd.sh.bash
+++ tests/helpers/libexec::cmd.sh.bash
@@ -7,5 +7,5 @@ bats::remote
 export GIT_DIRECTORY="${BATS_REMOTE[0]}/directory"
 export OUTPUT_FAILURE="top: "
 mkdir -p "${GIT_DIRECTORY}"
-OUTPUT_SUCCESS="${OUTPUT_FAILURE}$(cd "${GIT_DIRECTORY}" || return 1; git rev-parse --show-toplevel)"
+OUTPUT_SUCCESS="${OUTPUT_FAILURE}$(cd "${GIT_DIRECTORY}" || return 1; . cd.sh && cd_top && echo "${CD_TOP}")"
 export OUTPUT_SUCCESS
