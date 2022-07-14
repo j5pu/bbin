f00cf1899109a308a670f3583dd2087d12388d90
root <root@example.com>
2022-07-15 00:22:34 +0200

    evict

--- tmp/functions_macos.sh
+++ tmp/functions_macos.sh
@@ -28,7 +28,56 @@ export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 export HOMEBREW_BUNDLE_FILE="${HOME}/bbin/Brewfile"
 
 # JULIA - JUDICIAL
-download() { find -L "$(realpath "${1:-.}")" -type f -name "*.icloud" -exec brctl download "{}" \;; }
-evict() { find -L "$(realpath "${1:-.}")" -type f -not -name "*.icloud" -not -name ".DS_Store" -exec brctl evict "{}" \;; }
-preserve() { rsync -aptvADENUX "$@"; }
-
+download() { 
+  local dir
+  dir="$(realpath "${1:-.}")"
+  find -L "${dir}" -type d -exec brctl download "{}" \;
+  find -L "${dir}" -type f -name "*.icloud" -exec brctl download "{}" \;
+}
+evict() {
+  local dir
+  dir="$(realpath "${1:-.}")" 
+#  find -L "${dir}" -type d -exec brctl evict "{}" \;
+  find -L "${dir}" -type f -not -name "*.icloud" -not -name ".DS_Store" -exec brctl evict "{}" \;
+}
+preserve() { rsync -aptvADENUX --exclude "*.icloud" "$@"; }
+status() {
+  local i status x total
+  status="$(brctl status com.apple.CloudDocs)"
+  status_show() { echo "${3-}${1}: $(grep -- "$2" <<< "${status}" | wc -l)" ; }
+  for i in apply downloader reader sync-up upload; do
+    status_show "${i}" "> ${i}{"
+    case "${i}" in
+      apply) 
+        for x in pending-download pending-parent; do
+          status_show "${x}" "${x}" "  "
+        done
+        echo "--------------------------------"
+        ;;
+      downloader) 
+        for x in pending-disk; do
+          status_show "${x}" "${x}" "  "
+        done
+        ;;
+      reader)
+        status_show "needs-read[lost]" "needs-read" "  "
+        ;; 
+      sync-up) 
+        for x in sync-up-scheduled; do
+          status_show "${x}" "${x}" "  "
+        done
+        ;;
+      upload) 
+        for x in needs-sync-up needs-upload; do
+          status_show "${x}" "${x}" "  "
+        done
+        ;;
+    esac
+  done
+  echo "--------------------------------"
+  total="$(find -L "${HOME}/Library/Mobile Documents/com~apple~CloudDocs" -type f)"
+  echo "Total: $(wc -l <<< "${total}")"
+  echo "  evicted: $(wc -l < <(grep ".icloud$" <<< "${total}"))"
+  echo "  downloaded: $(wc -l < <(grep -v ".icloud$" <<< "${total}"))"
+  unset -f status_show
+}
