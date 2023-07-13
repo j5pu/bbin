0150223fbdecc162ba69a63a20fa1c75d3b6e4c9
root <root@example.com>
2023-07-13 16:04:34 +0200

    auto

--- tmp/functions.sh
+++ tmp/functions.sh
@@ -2,6 +2,8 @@
 alias ls="lsd"
 
 alias now="sudo shutdown -h now"
+alias starship-init='eval "$(starship init bash)"'
+eval $(thefuck --alias)
 
 # brew all
 bb() {
@@ -79,6 +81,38 @@ enable_sharing() {
   sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.smbd.plist
   sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server.plist EnabledServices -array disk
   test $# -eq 0 || sudo sharing -a "$@"
+}
+
+#######################################
+# create a native app from url
+# Arguments:
+#  url     required
+#  name
+#  icon
+# Examples:
+#  native laSexta /Users/j5pu/CloudDocs/Downloads/unnamed.png https://www.atresplayer.com/directos/lasexta
+#  native https://www.atresplayer.com/directos/lasexta
+#######################################
+native() {
+  local app params=() tmp url
+  tmp="$(mktemp -d)"
+  for arg; do
+    case "$arg" in
+      http*) url="$arg" ;;
+      *) if test -f "$arg"; then
+           params+=(--icon "$arg")
+         else
+           params+=(--name "$arg")
+         fi
+      ;;
+    esac
+  done
+  : "${url?url is required}"
+  nativefier --ignore-certificate --single-instance --darwin-dark-mode-support  "${params[@]}"  "$url" "$tmp"
+  app="$(find "$tmp" -type d -maxdepth 2 -name "*.app")"
+  mv "$app" "/Users/j5pu/Library/Mobile Documents/com~apple~CloudDocs/Applications/nativefier"
+  open "/Users/j5pu/Library/Mobile Documents/com~apple~CloudDocs/Applications/nativefier/${app##*/}"
+  rm -rf "$tmp"
 }
 
 #######################################
